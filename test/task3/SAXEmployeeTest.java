f56bb8565af02f325e6773f08971fe3cad13c42d
Georgi Georgiev <georgi.hristov@clouway.com>
2012-12-03 10:30:22 +0200

    Some sort of work around method :)

--- test/task3/SAXEmployeeTest.java
+++ test/task3/SAXEmployeeTest.java
@@ -1,5 +1,6 @@
 package task3;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
@@ -9,59 +10,74 @@ import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-import javax.xml.validation.Schema;
 import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author georgi.hristov@clouway.com
  */
 public class SAXEmployeeTest {
 
-  class SAXDefaultHandler extends DefaultHandler {
+  class Employee {
+    StringBuffer information = new StringBuffer();
 
 
+    public void addInformation(String newInformation) {
 
-    public SAXDefaultHandler() {
+      information.append(newInformation).append(" ");
+
+    }
+
+  }
+
+  class EmployeesInformationHandler extends DefaultHandler {
+
+    List<Employee> employees = Lists.newArrayList();
+    private int index = 0;
+
+    public EmployeesInformationHandler() {
 
       super();
 
     }
 
-    public void startDocument() {
+    public void startDocument() { }
 
-      System.out.println("START OF THE HELL");
-
-    }
-
-    public void endDocument() {
-
-      System.out.println("STILL NOT THERE YET");
-
-    }
+    public void endDocument() {}
 
     public void startElement(String uri, String name, String qName, Attributes atts) {
 
-
+      if (name.equals("employee") && index == 0) {
+        employees.add(new Employee());
+      } else if(name.equals("employee")){
+        employees.add(new Employee());
+        index++;
+      }
 
     }
 
     public void endElement(String uri, String name, String qName) {
     }
 
-    public void characters (char ch[], int start, int length)
-    {
-      String variable="";
+    public void characters(char ch[], int start, int length) {
 
+      Employee employee = employees.get(index);
+      StringBuilder stringBuilder = new StringBuilder();
       for (int i = start; i < start + length; i++) {
-
-           variable = variable+ch[i];
+        stringBuilder.append(ch[i]);
 
       }
-      System.out.println(variable);
+      employee.addInformation(stringBuilder.toString());
     }
 
+    public List<Employee> getEmployees(){
+
+      return  employees;
+
+    }
 
   }
 
@@ -69,10 +85,13 @@ public class SAXEmployeeTest {
   @Test
   public void playingWithSAX() throws SAXException, ParserConfigurationException, IOException {
     XMLReader reader = XMLReaderFactory.createXMLReader();
-    SAXDefaultHandler handler = new SAXDefaultHandler();
+    EmployeesInformationHandler handler = new EmployeesInformationHandler();
     reader.setContentHandler(handler);
     reader.setErrorHandler(handler);
     reader.parse(new InputSource(ClassLoader.getSystemResourceAsStream("employees.xml")));
+    List<Employee> employees = handler.getEmployees();
+    assertThat(employees.size(),is(2)) ;
+
 
   }
 
