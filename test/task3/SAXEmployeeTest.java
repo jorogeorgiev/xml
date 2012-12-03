25203d3ea5745b0b003212061a8f5f13905e02da
Georgi Georgiev <georgi.hristov@clouway.com>
2012-12-03 16:50:21 +0200

    Trying TDD by the book because i think that more appropriate , tried other way and i wrote mayn bullshits which were not needed at all.

--- test/task3/SAXEmployeeTest.java
+++ test/task3/SAXEmployeeTest.java
@@ -3,13 +3,13 @@ package task3;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.List;
 
@@ -22,73 +22,32 @@ import static org.junit.Assert.assertThat;
 
 public class SAXEmployeeTest {
 
-
-  class EmployeesInformationHandler extends DefaultHandler {
-
-    List<Employee> employees = Lists.newArrayList();
-    List<String> employeePersonalInformation;
-    private int employeeIndex = 0;
-
-    public EmployeesInformationHandler() {
-
-      super();
-
-    }
-
-    public void startDocument() {
-
-    }
-
-    public void endDocument() {
-    }
-
-    public void startElement(String uri, String name, String qName, Attributes atts) {
-
-      if (name.equals("employee")) {
-        createEmployee();
-      }
-
-
-    }
-
-    private void createEmployee() {
-      employees.add(new Employee());
-
-      employeePersonalInformation = Lists.newArrayList();
-      employeeIndex++;
-    }
-
-    public void endElement(String uri, String name, String qName) {
-
-    }
-
-    public void characters(char ch[], int start, int length) {
-      StringBuilder builder = new StringBuilder();
-      for (int i = start; i < start + length; i++) {
-        builder.append(ch[i]);
-      }
-      employeePersonalInformation.add(builder.toString());
-      employees.get(employeeIndex - 1).addPersonalInformation(employeePersonalInformation);
-
-    }
-
-    public List<Employee> getEmployees() {
-      return employees;
-    }
+  class Employee {
   }
 
+  class EmployeeContentHandler extends DefaultHandler {
+
+    private List<Employee> employees;
+
+    public EmployeeContentHandler(List<Employee> employees){
+      super();
+      this.employees = employees;
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+
+
+    }
+  }
 
   @Test
-  public void playingWithSAX() throws SAXException, ParserConfigurationException, IOException {
-
-    XMLReader reader = XMLReaderFactory.createXMLReader();
-    EmployeesInformationHandler handler = new EmployeesInformationHandler();
-    reader.setContentHandler(handler);
-    reader.setErrorHandler(handler);
-    reader.parse(new InputSource(ClassLoader.getSystemResourceAsStream("employees.xml")));
-    assertThat(handler.getEmployees().size(), is(2));
-
+  public void createTwoEmployeeObjectsFromXML() throws SAXException, IOException {
+    XMLReader xmlReader = XMLReaderFactory.createXMLReader();
+    xmlReader.setContentHandler(new EmployeeContentHandler());
+    xmlReader.parse(new InputSource(ClassLoader.getSystemResourceAsStream("employees.xml")));
+    List<Employee> employees = Lists.newArrayList();
+    assertThat(employees.size(), is(2));
   }
-
 
 }
