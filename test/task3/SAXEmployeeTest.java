8526b4635fac5b616cefbb4bc144d38279edb19f
Georgi Georgiev <georgi.hristov@clouway.com>
2012-12-03 12:16:30 +0200

    Working version of SAX  parsing Employee XML. Must remove duplication with polymorphism , make it little bt more configurable, write some tests and renaming..

--- test/task3/SAXEmployeeTest.java
+++ test/task3/SAXEmployeeTest.java
@@ -1,6 +1,7 @@
 package task3;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.junit.Test;
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
@@ -12,6 +13,7 @@ import org.xml.sax.helpers.XMLReaderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -22,12 +24,73 @@ import static org.junit.Assert.assertThat;
 public class SAXEmployeeTest {
 
   class Employee {
-    StringBuffer information = new StringBuffer();
+
+    private List<String> personalInformation;
+    private List<Employer> employers;
+    private List<Address> address;
+
+    public void addPersonalInformation(List<String> personalInformation) {
+      this.personalInformation = personalInformation;
+    }
+
+    public void addEmployerInformation(List<Employer> employers) {
+      this.employers = employers;
+    }
+
+    public void addAddressInformation(List<Address> address) {
+      this.address = address;
+    }
+
+    public void printInfo() {
+      for (String data : personalInformation) {
+        System.out.println(data + " ");
+      }
+      for (Employer employer : employers) {
+        employer.pritnInfo();
+      }
+      for (Address addr : address) {
+        addr.printInfo();
+      }
 
 
-    public void addInformation(String newInformation) {
+    }
 
-      information.append(newInformation).append(" ");
+  }
+
+  class Employer {
+
+    private List<String> employersInformation;
+
+    public void addEmployersInformation(List<String> employersInformation) {
+
+      this.employersInformation = employersInformation;
+    }
+
+    public void pritnInfo() {
+      for (String data : employersInformation) {
+
+        System.out.println(data + " ");
+
+      }
+    }
+
+  }
+
+  class Address {
+
+    private List<String> addressInformation;
+
+    public void addAddressInformation(List<String> addressInformation) {
+
+      this.addressInformation = addressInformation;
+    }
+
+    public void printInfo() {
+      for (String data : addressInformation) {
+
+        System.out.println(data + " ");
+
+      }
 
     }
 
@@ -35,8 +98,22 @@ public class SAXEmployeeTest {
 
   class EmployeesInformationHandler extends DefaultHandler {
 
-    List<Employee> employees = Lists.newArrayList();
-    private int index = 0;
+
+    Map<String, Boolean> attributeLocks = Maps.newHashMap();
+
+    List<String> employeePersonalInformation;
+    Map<Integer, Employee> employees = Maps.newHashMap();
+    private int employeeIndex = 1;
+
+    List<String> employersInformation;
+    List<Employer> employerList;
+    Map<Integer, Employer> employers = Maps.newHashMap();
+    private int employersIndex = 1;
+
+    List<Address> addressList;
+    List<String> addressInformation;
+    Map<Integer, Address> addresses = Maps.newHashMap();
+    private int addressesIndex = 1;
 
     public EmployeesInformationHandler() {
 
@@ -44,39 +121,98 @@ public class SAXEmployeeTest {
 
     }
 
-    public void startDocument() { }
+    public void startDocument() {
+      attributeLocks.put("employee", false);
+      attributeLocks.put("employer", false);
+      attributeLocks.put("address", false);
+    }
 
-    public void endDocument() {}
+    public void endDocument() {
+    }
 
     public void startElement(String uri, String name, String qName, Attributes atts) {
 
-      if (name.equals("employee") && index == 0) {
-        employees.add(new Employee());
-      } else if(name.equals("employee")){
-        employees.add(new Employee());
-        index++;
+      if (name.equals("employee")) {
+        createEmployee();
+        attributeLocks.put(name, true);
       }
 
+      if (name.equals("employer")) {
+        createEmployer();
+        attributeLocks.put(name, true);
+      }
+
+      if (name.equals("address")) {
+        createAddress();
+        attributeLocks.put(name, true);
+      }
+
+    }
+
+    private void createEmployee() {
+      employeePersonalInformation = Lists.newArrayList();
+      employees.put(employeeIndex, new Employee());
+      employeeIndex++;
+    }
+
+    private void createEmployer() {
+      employerList = Lists.newArrayList();
+      employersInformation = Lists.newArrayList();
+      employers.put(employersIndex, new Employer());
+      employersIndex++;
+    }
+
+    private void createAddress() {
+      addressList = Lists.newArrayList();
+      addressInformation = Lists.newArrayList();
+      addresses.put(addressesIndex, new Address());
+      addressesIndex++;
     }
 
     public void endElement(String uri, String name, String qName) {
     }
 
     public void characters(char ch[], int start, int length) {
-
-      Employee employee = employees.get(index);
-      StringBuilder stringBuilder = new StringBuilder();
+      StringBuilder builder = new StringBuilder();
       for (int i = start; i < start + length; i++) {
-        stringBuilder.append(ch[i]);
+        builder.append(ch[i]);
+      }
+      for (Map.Entry<String, Boolean> locks : attributeLocks.entrySet()) {
+
+
+        if (locks.getValue()) {
+
+          if (locks.getKey().equals("employee")) {
+            employeePersonalInformation.add(builder.toString());
+            employees.get(employeeIndex - 1).addPersonalInformation(employeePersonalInformation);
+          }
+
+          if (locks.getKey().equals("employer")) {
+            employersInformation.add(builder.toString());
+            employers.get(employersIndex - 1).addEmployersInformation(employersInformation);
+            employees.get(employeeIndex - 1).addEmployerInformation(employerList);
+          }
+
+          if (locks.getKey().equals("address")) {
+            addressInformation.add(builder.toString());
+            addresses.get(addressesIndex - 1).addAddressInformation(addressInformation);
+            employees.get(employeeIndex - 1).addAddressInformation(addressList);
+          }
+
+
+        }
 
       }
-      employee.addInformation(stringBuilder.toString());
+
+
     }
 
-    public List<Employee> getEmployees(){
 
-      return  employees;
-
+    public void printEmployesInformation() {
+      for (Employee employee : employees.values()) {
+        employee.printInfo();
+        System.out.println("<---------END OF USERS INFO--------->");
+      }
     }
 
   }
@@ -89,10 +225,7 @@ public class SAXEmployeeTest {
     reader.setContentHandler(handler);
     reader.setErrorHandler(handler);
     reader.parse(new InputSource(ClassLoader.getSystemResourceAsStream("employees.xml")));
-    List<Employee> employees = handler.getEmployees();
-    assertThat(employees.size(),is(2)) ;
-
-
+    handler.printEmployesInformation();
   }
 
 
