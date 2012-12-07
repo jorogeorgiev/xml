3da6a5bd641aaa5d1b8ef6cc4789243ede99a81f
Georgi Georgiev <georgi.hristov@clouway.com>
2012-12-07 10:17:30 +0200

    While learning how ErroHandler handles the error i made a whole implementation of the handler for furtheruse.

--- test/task3/ErrorHandlerLearningTest.java
+++ test/task3/ErrorHandlerLearningTest.java
@@ -1,5 +1,6 @@
 package task3;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.InputSource;
@@ -8,7 +9,12 @@ import org.xml.sax.SAXParseException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 import java.io.IOException;
+import java.io.InputStream;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -19,9 +25,24 @@ import static org.mockito.Mockito.verify;
  */
 public class ErrorHandlerLearningTest {
 
+  ErrorPrompt prompt;
+  ErrorMessages message;
+  MyErrorHandler handler;
+  InputStream xmlData ;
+
+  @Before
+  public void setUp(){
+    prompt = mock(ErrorPrompt.class);
+    message = new ErrorMessages();
+    handler = new MyErrorHandler(prompt, message);
+    xmlData = ErrorHandlerLearningTest.class.getResourceAsStream("invalidXML.xml");
+  }
+
+
+
   interface ErrorPrompt {
 
-    void promptError(String message);
+    void prompt(String message);
 
   }
 
@@ -29,19 +50,19 @@ public class ErrorHandlerLearningTest {
 
     public String onWarning(Integer line, Integer column, String message) {
 
-      return "Warning on line: " + line + " and column: " + column + "warning: " + message;
+      return "Warning on line:" + line + " and column:" + column + " error: " + message;
 
     }
 
     public String onError(Integer line, Integer column, String message) {
 
-      return "Error on line: " + line + " and column: " + column + "warning: " + message;
+      return "Error on line:" + line + " and column:" + column + " error: " + message;
 
     }
 
     public String onCriticalError(Integer line, Integer column, String message) {
 
-      return "Critical error on line: " + line + " and column: " + column + "warning: " + message;
+      return "Critical error on line:" + line + " and column:" + column + " error: " + message;
 
     }
 
@@ -51,53 +72,76 @@ public class ErrorHandlerLearningTest {
 
     private ErrorPrompt prompt;
 
-    private ErrorMessages messages;
+    private ErrorMessages errorMessage;
 
     public MyErrorHandler(ErrorPrompt prompt, ErrorMessages messages) {
 
       this.prompt = prompt;
 
-      this.messages = messages;
+      this.errorMessage = messages;
 
     }
 
 
     @Override
-    public void warning(SAXParseException exception){
+    public void warning(SAXParseException exception) {
 
-      prompt.promptError(messages.onWarning(exception.getLineNumber(), exception.getColumnNumber(), exception.getMessage()));
-
-    }
-
-    @Override
-    public void error(SAXParseException exception){
-
-      prompt.promptError(messages.onError(exception.getLineNumber(), exception.getColumnNumber(), exception.getMessage()));
+      prompt.prompt(errorMessage.onWarning(exception.getLineNumber(), exception.getColumnNumber(), exception.getMessage()));
 
     }
 
     @Override
-    public void fatalError(SAXParseException exception){
+    public void error(SAXParseException exception) {
 
-      prompt.promptError(messages.onCriticalError(exception.getLineNumber(), exception.getColumnNumber(), exception.getMessage()));
+      prompt.prompt(errorMessage.onError(exception.getLineNumber(), exception.getColumnNumber(), exception.getMessage()));
+
+    }
+
+    @Override
+    public void fatalError(SAXParseException exception) {
+
+      prompt.prompt(errorMessage.onCriticalError(exception.getLineNumber(), exception.getColumnNumber(), exception.getMessage()));
 
     }
 
   }
 
   @Test
-  public void errorHandlerNotifiesOnTagErrorDuringParsing() throws IOException, SAXException {
+  public void errorHandlerNotifiesOnTagErrorDuringParsing() throws IOException {
 
-    ErrorPrompt prompt = mock(ErrorPrompt.class);
-    ErrorMessages message = new ErrorMessages();
-    MyErrorHandler handler = new MyErrorHandler(prompt, message);
-    XMLReader reader = XMLReaderFactory.createXMLReader();
-    reader.setErrorHandler(handler);
+    parseXML(xmlData);
 
-    reader.parse(new InputSource(ErrorHandlerLearningTest.class.getResourceAsStream("invalidXML.xml")));
-     verify(prompt,times(1)).promptError(message.onCriticalError(20,6,"The element type \"employer\" must be terminated by the matching end-tag \"</employer>\"."));
+    verify(prompt, times(1)).prompt(message.onCriticalError(32, 25, "The element type \"fistName\" must be terminated by the matching end-tag \"</fistName>\"."));
+  }
+
+  @Test
+  public void errorHandlerNotifiesOnInvalidSequenceDuringValidation() throws SAXException, IOException {
+
+    validateXML(xmlData);
+
+    verify(prompt, times(1)).prompt(message.onError(18, 15, "cvc-complex-type.2.4.a: Invalid content was found starting with element 'position'. One of '{endDate}' is expected."));
 
 
+  }
+
+  private void parseXML(InputStream xmlStream) {
+    try {
+      XMLReader reader = XMLReaderFactory.createXMLReader();
+      reader.setErrorHandler(handler);
+      reader.parse(new InputSource(xmlStream));
+    } catch (SAXException e) {
+    } catch (IOException e) {
+    }
+  }
+
+  private void validateXML(InputStream xmlStream) {
+    try {
+      Validator validator = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(new StreamSource(ErrorHandlerLearningTest.class.getResourceAsStream("schema.xsd"))).newValidator();
+      validator.setErrorHandler(handler);
+      validator.validate(new StreamSource(xmlStream));
+    } catch (SAXException e) {
+    } catch (IOException e) {
+    }
   }
 
 
